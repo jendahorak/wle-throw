cdad20dcdcfe1fdd522a300e22ab4d02406a3ae2
Jan Horák <7425857-shtrdl@users.noreply.gitlab.com>
2023-12-10 11:42:20 +0100

    .

---
+++ cache/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _wle_pp from 'wle-pp';
+_registerEditor(_wle_pp);
---
+++ js/index.js
@@ -0,0 +1,108 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-imports:start` and `wle:auto-imports:end`: The list of import statements
+ *     - `wle:auto-register:start` and `wle:auto-register:end`: The list of component to register
+ *     - `wle:auto-constants:start` and `wle:auto-constants:end`: The project's constants,
+ *        such as the project's name, whether it should use the physx runtime, etc...
+ *     - `wle:auto-benchmark:start` and `wle:auto-benchmark:end`: Append the benchmarking code
+ */
+
+/* wle:auto-imports:start */
+import {MouseLookComponent} from '@wonderlandengine/components';
+import {WasdControlsComponent} from '@wonderlandengine/components';
+import {ConsoleVRToolComponent} from 'wle-pp';
+import {EasyTuneToolComponent} from 'wle-pp';
+import {GamepadMeshAnimatorComponent} from 'wle-pp';
+import {GrabbableComponent} from 'wle-pp';
+import {GrabberHandComponent} from 'wle-pp';
+import {PPGatewayComponent} from 'wle-pp';
+import {SetHandLocalTransformComponent} from 'wle-pp';
+import {SetHeadLocalTransformComponent} from 'wle-pp';
+import {SpatialAudioListenerComponent} from 'wle-pp';
+import {SwitchHandObjectComponent} from 'wle-pp';
+import {ToolCursorComponent} from 'wle-pp';
+import {TrackedHandDrawAllJointsComponent} from 'wle-pp';
+/* wle:auto-imports:end */
+
+import { loadRuntime } from '@wonderlandengine/api';
+
+/* wle:auto-constants:start */
+const RuntimeOptions = {
+    physx: true,
+    loader: false,
+    xrFramebufferScaleFactor: 1,
+    canvas: 'canvas',
+};
+const Constants = {
+    ProjectName: '006_physics',
+    RuntimeBaseName: 'WonderlandRuntime',
+    WebXRRequiredFeatures: ['local',],
+    WebXROptionalFeatures: ['local','hand-tracking','hit-test',],
+};
+/* wle:auto-constants:end */
+
+const engine = await loadRuntime(Constants.RuntimeBaseName, RuntimeOptions);
+
+engine.onSceneLoaded.once(() => {
+    const el = document.getElementById('version');
+    if (el) setTimeout(() => el.remove(), 2000);
+});
+
+/* WebXR setup. */
+
+function requestSession(mode) {
+    engine
+        .requestXRSession(mode, Constants.WebXRRequiredFeatures, Constants.WebXROptionalFeatures)
+        .catch((e) => console.error(e));
+}
+
+function setupButtonsXR() {
+    /* Setup AR / VR buttons */
+    const arButton = document.getElementById('ar-button');
+    if (arButton) {
+        arButton.dataset.supported = engine.arSupported;
+        arButton.addEventListener('click', () => requestSession('immersive-ar'));
+    }
+    const vrButton = document.getElementById('vr-button');
+    if (vrButton) {
+        vrButton.dataset.supported = engine.vrSupported;
+        vrButton.addEventListener('click', () => requestSession('immersive-vr'));
+    }
+}
+
+if (document.readyState === 'loading') {
+    window.addEventListener('load', setupButtonsXR);
+} else {
+    setupButtonsXR();
+}
+
+/* wle:auto-register:start */
+engine.registerComponent(MouseLookComponent);
+engine.registerComponent(WasdControlsComponent);
+engine.registerComponent(ConsoleVRToolComponent);
+engine.registerComponent(EasyTuneToolComponent);
+engine.registerComponent(GamepadMeshAnimatorComponent);
+engine.registerComponent(GrabbableComponent);
+engine.registerComponent(GrabberHandComponent);
+engine.registerComponent(PPGatewayComponent);
+engine.registerComponent(SetHandLocalTransformComponent);
+engine.registerComponent(SetHeadLocalTransformComponent);
+engine.registerComponent(SpatialAudioListenerComponent);
+engine.registerComponent(SwitchHandObjectComponent);
+engine.registerComponent(ToolCursorComponent);
+engine.registerComponent(TrackedHandDrawAllJointsComponent);
+/* wle:auto-register:end */
+
+let loadDelaySeconds = 0;
+if (loadDelaySeconds > 0) {
+    setTimeout(() => engine.scene.load(`${Constants.ProjectName}.bin`), loadDelaySeconds * 1000);
+} else {
+    engine.scene.load(`${Constants.ProjectName}.bin`);
+}
+
+/* wle:auto-benchmark:start */
+/* wle:auto-benchmark:end */
